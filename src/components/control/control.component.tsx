6644727943baa670b85b0e30b44c6c14ee6a0527
Francisco <frandelosantoss12@gmail.com>
2023-10-09 14:02:27 -0300

    Fix wrong import in control component

--- src/components/control/control.component.tsx
+++ src/components/control/control.component.tsx
@@ -1,41 +1,31 @@
-import { useField } from 'formik'
+import { FieldInputProps, useField } from 'formik'
 
-import type { ControlComponent } from 'models'
-
+import { FC } from 'react'
 import styles from './control.component.module.scss'
 
-const Control: ControlComponent = props => {
-    const [
-        field,
-        meta
-    ] = useField(props)
-    
-    return (
-        <div
-            className={styles.control}>
-            <input
-                className={styles.input}
-                id={props.name}
-                placeholder=" "
-                {...field}
-                {...props}
-            />
-            <label
-                className={styles.label}
-                htmlFor={props.name}>
-                {props.name}
-            </label>
-            {
-                (meta.touched && meta.error)
-                && (
-                    <p
-                        className={styles.error}>
-                        {meta.error}
-                    </p>
-                )
-            }
-        </div>
-    )
+const Control: FC<FieldInputProps<any>> = (props) => {
+	const [field, meta] = useField(props)
+
+	return (
+		<div className={styles.control}>
+			<input
+				className={styles.input}
+				id={props.name}
+				placeholder=' '
+				{...field}
+				{...props}
+			/>
+			<label
+				className={styles.label}
+				htmlFor={props.name}
+			>
+				{props.name}
+			</label>
+			{meta.touched && meta.error && (
+				<p className={styles.error}>{meta.error}</p>
+			)}
+		</div>
+	)
 }
 
 export default Control
