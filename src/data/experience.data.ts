fef945c2a6384477bdc22b1eb0212d5886ee9899
Francisco <frandelosantoss12@gmail.com>
2024-03-11 23:07:41 -0300

    Add DashRD experience

--- src/data/experience.data.ts
+++ src/data/experience.data.ts
@@ -2,6 +2,15 @@ import { Experience } from 'models'
 
 const experience: Experience[] = [
 	{
+		title: 'DashRD | Web developer',
+		period: 'November 2023 - Present (Currently working)',
+		link: '/',
+		logo: {
+			src: 'assets/experience/dashrd.avif',
+			alt: 'DashRD logo',
+		},
+	},
+	{
 		title: 'Upwork | Freelancer Web Developer',
 		period: 'February 2022 - July 2022',
 		link: 'https://www.upwork.com/freelancers/~0152beea25154149fa',
